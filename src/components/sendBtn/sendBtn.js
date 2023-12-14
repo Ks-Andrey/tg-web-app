e6475931f1e81c62b01992808a6e4156fd86038c
Andrey Kasach <kasach.andrey123@mail.com>
2023-12-15 01:34:27 +0300

    solve problem

--- src/components/sendBtn/sendBtn.js
+++ src/components/sendBtn/sendBtn.js
@@ -2,20 +2,16 @@ import useTelegram from '../../hooks/useTelegram/useTelegram';
 
 import './sendBtn.css'
 
-const SendButton = ({totalLength, totalPrice, bakset}) => {
-    const { tg, onClose } = useTelegram();
+const SendButton = ({totalLength, totalPrice}) => {
+    const { onClose } = useTelegram();
 
     const sendData = async () => {
-        await tg.ready();
-
-        tg.sendData(JSON.stringify(bakset));
         onClose();
     }
 
     return (
         <button onClick={sendData} className="send__data">
           Оформить заказ из {totalLength} товаров на {totalPrice} руб
-          {tg.initDataUnsafe?.user}
         </button>
     );
 }
